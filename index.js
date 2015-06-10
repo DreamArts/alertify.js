2a5de23225935d8efa5daab780ac574b44542b61
exabugs <exabugs@gmail.com>
2015-06-10 17:43:07 +0900

    webpack

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+/**
+ * exabugs 2015/06/10
+ * DreamArts Corp.
+ */
+
 require('!style!css!./themes/alertify.bootstrap.css');
 require('!style!css!./themes/alertify.core.css');
-require('!style!css!./themes/alertify.default.css');
-require('./lib/alertify');
 
 // With no changes to the original source we must export from the global object
-module.exports = window.Alertify;
+module.exports = require("imports?this=>window!./lib/alertify");
