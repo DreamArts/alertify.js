ed95f37cd73e257a006c86e2bf169b1d0e7af7e1
exabugs <exabugs@gmail.com>
2015-06-13 15:45:14 +0900

    webpack

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  * DreamArts Corp.
  */
 
+"use strict";
+
 require('!style!css!./themes/alertify.bootstrap.css');
 require('!style!css!./themes/alertify.core.css');
+
 module.exports = require("imports?this=>window!./lib/alertify");
